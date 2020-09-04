ba39100aeeeb7d073af83cfd25d78b23f417d0d1
Mahabubur Rahman <mahabuburr01625@gmail.com>
2020-09-04 16:17:38 +0600

    added note taking feature

---
+++ js/main.js
@@ -0,0 +1,21 @@
+// Varitables
+let form = document.getElementById('takeNote');
+let notes = document.querySelector('#notes .container');
+
+// Functions
+const takeNote = (e) => {
+  e.preventDefault();
+  let noteContent = document.getElementById('noteContent').value;
+  let note = document.createElement('div');
+  note.className = 'note';
+
+  let body = document.createElement('p');
+  body.className = 'body';
+  body.appendChild(document.createTextNode(noteContent));
+
+  note.appendChild(body);
+  notes.appendChild(note);
+};
+
+// Events
+form.addEventListener('submit', takeNote);
