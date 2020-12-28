6e0d7aef82c3442e1f658cc96ac330e3248b863f
Mahabubur Rahman <microronyy@gmail.com>
2020-12-28 19:49:22 +0600

    update

--- js/main.js
+++ js/main.js
@@ -5,8 +5,8 @@ let notes = document.querySelector('#notes .container');
 // Functions
 const takeNote = (e) => {
   e.preventDefault();
-  let noteContent = document.getElementById('noteContent').value;
   let note = document.createElement('div');
+  let noteContent = document.getElementById('noteContent').value;
   note.className = 'note';
 
   let body = document.createElement('p');
